ebe7467437eb20fb2f84cc27055e8ab494d75ac0
Inigo Joaquin S Natividad <inigojoaquin@R.local>
2025-06-13 22:33:49 +0800

    first commit

---
+++ set_1.py
@@ -0,0 +1,10 @@
+def savings(gross_pay, tax_rate, expenses):
+    return(gross_pay - expenses - gross_pay*tax_rate)
+
+
+def material_waste(total_material, material_units, num_jobs, job_consumption):
+    return (str(total_material - num_jobs*job_consumption)+str(material_units))
+
+
+def interest(principal, rate, periods):
+    return (int(principal+principal*rate*periods))
